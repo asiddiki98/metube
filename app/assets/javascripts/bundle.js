b39d5ba6f04d0fc28bb67d44f1e2388959785d4b
asiddiki98 <azimsardines@gmail.com>
2021-10-22 17:16:46 -0400

    app bug in frontend entry file

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -29863,7 +29863,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
 
 
-document.addEventListener("DOMContentLoaded", function () {
+ocument.addEventListener("DOMContentLoaded", function () {
   var root = document.getElementById("root");
   react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "helo"), root);
 });
--- frontend/metube.jsx
+++ frontend/metube.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-document.addEventListener("DOMContentLoaded", () => {
+ocument.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root")
     ReactDOM.render(<h1>helo</h1>, root)
 })
